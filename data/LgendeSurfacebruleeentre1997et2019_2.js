4452c6d6c34547eabac31fe100b04dfc8e29c008
SweetPizzaTo <74381627+SweetPizzaTo@users.noreply.github.com>
2020-11-30 22:51:15 +0100

    Add files via upload

---
+++ data/LgendeSurfacebruleeentre1997et2019_2.js
@@ -0,0 +1,19 @@
+var json_LgendeSurfacebruleeentre1997et2019_2 = {
+"type": "FeatureCollection",
+"name": "ponct",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": {  "code_region": 1, "q2wHide_SurfBrul97": 287 }, "geometry": { "type": "Point", "coordinates": [ -5.174870945868815, 35.144355670237367 ] } },
+{ "type": "Feature", "properties": {  "code_region": 2, "q2wHide_SurfBrul97": 183 }, "geometry": { "type": "Point", "coordinates": [ -2.660228717590605, 33.51985831470197 ] } },
+{ "type": "Feature", "properties": {  "code_region": 4, "q2wHide_SurfBrul97": 233 }, "geometry": { "type": "Point", "coordinates": [ -6.208403177146321, 34.063224330972446 ] } },
+{ "type": "Feature", "properties": {  "code_region": 5, "q2wHide_SurfBrul97": 255 }, "geometry": { "type": "Point", "coordinates": [ -6.299699062670522, 32.456149821282139 ] } },
+{ "type": "Feature", "properties": {  "code_region": 7, "q2wHide_SurfBrul97": 162 }, "geometry": { "type": "Point", "coordinates": [ -8.456413702183118, 31.69999817736452 ] } },
+{ "type": "Feature", "properties": {  "code_region": 3, "q2wHide_SurfBrul97": 271 }, "geometry": { "type": "Point", "coordinates": [ -4.839655259145196, 33.601399623691179 ] } },
+{ "type": "Feature", "properties": {  "code_region": 8, "q2wHide_SurfBrul97": 102 }, "geometry": { "type": "Point", "coordinates": [ -5.35410742431013, 31.201682567887786 ] } },
+{ "type": "Feature", "properties": {  "code_region": 9, "q2wHide_SurfBrul97": 57 }, "geometry": { "type": "Point", "coordinates": [ -8.335649485089402, 29.918830545839409 ] } },
+{ "type": "Feature", "properties": {  "code_region": 10, "q2wHide_SurfBrul97": 51 }, "geometry": { "type": "Point", "coordinates": [ -9.949117723497039, 28.342438893048456 ] } },
+{ "type": "Feature", "properties": { "code_region": 11, "q2wHide_SurfBrul97": 51 }, "geometry": { "type": "Point", "coordinates": [ -12.008479209177274, 26.318697742711091 ] } },
+{ "type": "Feature", "properties": {  "code_region": 12, "q2wHide_SurfBrul97": 38 }, "geometry": { "type": "Point", "coordinates": [ -14.464688743930314, 22.878439508120241 ] } },
+{ "type": "Feature", "properties": {  "code_region": 6, "q2wHide_SurfBrul97": 176 }, "geometry": { "type": "Point", "coordinates": [ -7.772910146534849, 33.002469685095299 ] } }
+]
+}
